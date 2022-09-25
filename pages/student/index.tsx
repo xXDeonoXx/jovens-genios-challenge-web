f059a61b0391ba19647c226a494d122438a3c81d
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2022-09-25 01:26:36 -0300

    feat: base student and teacher pages

---
+++ pages/student/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Student: NextPage = () => {
+  return <div className='w-screen h-screen bg-fill'>Student</div>;
+};
+
+export default Student;
---
+++ pages/teacher/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Teacher: NextPage = () => {
+  return <div className='w-screen h-screen bg-fill'>Teacher</div>;
+};
+
+export default Teacher;
